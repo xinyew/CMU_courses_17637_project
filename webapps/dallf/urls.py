390a6df9bfd83e338677257dad95ab55ea04f67f
english5040 <ejwu@andrew.cmu.edu>
2022-11-20 21:06:07 -0500

    Move urls to top-level

--- webapps/dallf/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from dallf import views
-
-urlpatterns = [
-    path('console/', views.console, name='console'),
-    path('gallery/', views.gallery, name='gallery'),
-]
--- webapps/webapps/urls.py
+++ webapps/webapps/urls.py
@@ -22,8 +22,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.gallery),  # Temporary
-    path('dallf/', include('dallf.urls')),
     path('oauth/', include('social_django.urls', namespace='social')),
+    path('console/', views.console, name='console'),
+    path('gallery/', views.gallery, name='gallery'),
     path('favorite/', views.favorite_action, name='favorite'),
     path('label/', views.label_action, name='label'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
