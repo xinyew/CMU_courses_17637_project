14915077bebb3875fdd801be8f572b205709ad6e
english5040 <ejwu@andrew.cmu.edu>
2022-12-15 16:35:57 -0500

    Revert settings

--- webapps/webapps/settings.py
+++ webapps/webapps/settings.py
@@ -29,7 +29,7 @@ development = env.bool('DEVELOPMENT', default=False)
 SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.str('DEBUG', False)
 
 ALLOWED_HOSTS = ['*']
 
